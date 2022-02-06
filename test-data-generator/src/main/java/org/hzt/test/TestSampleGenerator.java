1243be6ccebfe29db75c95e1feefa3fb96ed5a35
HZUIDERV <hans.zuidervaart@sogeti.com>
2022-02-06 23:27:45 +0100

    - test sample generator update

--- test-data-generator/src/main/java/org/hzt/test/TestSampleGenerator.java
+++ test-data-generator/src/main/java/org/hzt/test/TestSampleGenerator.java
@@ -13,6 +13,7 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.time.Year;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;

53368b0125eaaf8648e6ae74b5a45e5660344c87
HZUIDERV <hans.zuidervaart@sogeti.com>
2022-02-15 12:27:20 +0100

    - added exports clauses to module-info.java

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -3,5 +3,17 @@ open module core {
     requires org.jetbrains.annotations;
     requires java.desktop;
 
-    exports hzt;
+    exports hzt.collections;
+    exports hzt.function;
+    exports hzt.stream;
+    exports hzt.sequences;
+    exports hzt.statistics;
+    exports hzt.strings;
+    exports hzt.numbers;
+    exports hzt.io;
+    exports hzt.iterables;
+    exports hzt.ranges;
+    exports hzt.tuples;
+    exports hzt.utils;
+    exports hzt.collectors;
 }

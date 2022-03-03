a376e829b2a56b58e202875e03cc0f6df4fe6a32
HZUIDERV <hans.zuidervaart@sogeti.com>
2022-03-03 16:16:39 +0100

    - updated version all projects to java 11 - added swing-utils module

---
+++ swing-utils/src/test/java/org/hzt/swing_utils/function/window_listeners/WindowDeactivatedListenerTest.java
@@ -0,0 +1,36 @@
+package org.hzt.swing_utils.function.window_listeners;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.Frame;
+import java.awt.Window;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class WindowDeactivatedListenerTest {
+
+    @Test
+    void testWindowDeactivatedListener() {
+        final var thread = new Thread(this::testWindowDeactivated);
+        thread.start();
+        await().until(() -> !thread.isAlive());
+    }
+
+    private void testWindowDeactivated() {
+        AtomicBoolean isCalled = new AtomicBoolean(false);
+        Window frame = new Frame();
+        frame.addWindowListener((WindowDeactivatedListener) e -> isCalled.set(true));
+        frame.setVisible(true);
+        frame.setVisible(false);
+
+        System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
+
+        frame.dispose();
+
+        await().until(() -> !frame.isDisplayable());
+
+        assertTrue(isCalled.get());
+    }
+}

a45e0e6046238c57f1425be3df7b8ec1864a7009
HZUIDERV <hans.zuidervaart@sogeti.com>
2022-05-18 13:25:38 +0200

    - added exports clause in module-info.java of swing utils

--- swing-utils/src/main/java/module-info.java
+++ swing-utils/src/main/java/module-info.java
@@ -6,5 +6,6 @@ open module hzt.utils.swing {
     exports org.hzt.swing_utils.function.document_listeners;
     exports org.hzt.swing_utils.function.window_listeners;
     exports org.hzt.swing_utils.function.mouse_listeners;
+    exports org.hzt.swing_utils.builders;
 
 }
